b3a9d999d8cc1970a6b7bf3defd265ae76baf468
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-21 04:44:34 -0600

    Update elliptic_curve.py

--- main/src/elliptic_curve.py
+++ main/src/elliptic_curve.py
@@ -2,6 +2,7 @@ import os
 import sys
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.asymmetric import utils
 
 # ANS X9.62 defines methods for digital signature generation and verification using the Elliptic 
@@ -12,6 +13,8 @@ from cryptography.hazmat.primitives.asymmetric import utils
 #
 # Note that while elliptic curve keys can be used for both signing and key exchange, this is bad cryptographic practice. 
 # Instead, users should generate separate signing and ECDH keys.
+#
+# ECDSA keys shall not be used for any other purpose (e.g., key establishment)
 
 class Elliptic_Curve_Signature_Algorithms():
     def __init__(self):
@@ -58,4 +61,61 @@ class Elliptic_Curve_Signature_Algorithms():
             return public_key.verify(signature, digest, ec.ECDSA(utils.Prehashed(self.hash)))
 
 
+# This example does not give forward secrecy and is only provided as a demonstration of the 
+# basic Diffie-Hellman construction. 
+# For real world applications always use the ephemeral form described after this example.
+
+class Elliptic_Curve_Key_Exchange_algorithm():
+    def __init__(self):
+        self.exchange_algorthim = True
+        self.hash_algorithm     = hashes.SHA256()
+        self.key_length         = 32   # can be of any length (e.g, 1024, 2048, 256 and so on)
+        self.data_to_encode     = bytes('handshake data')
     
+    def generate_server_private_key(self, generate_server_private_key = True):
+        if generate_server_private_key:
+            server_private_key = ec.generate_private_key(ec.SECP384R1())
+        return server_private_key
+    
+    # In a real handshake the peer is a remote client. For this
+    # example we'll generate another local private key though.
+
+    def generate_peer_private_key(self, generate_peer_private_key = True):
+        if generate_peer_private_key:
+            peer_private_key = ec.generate_private_key(ec.SECP384R1())
+        return peer_private_key
+    
+    def shared_key_exchange(self, server_private_key, peer_private_key):
+        if server_private_key:
+            shared_key = server_private_key.exchange(ec.ECDH(), peer_private_key.public_key())
+        return shared_key
+    
+    # Perform key derivation.
+
+    def perform_key_derivation(self, shared_key):
+        if shared_key:
+            derived_key = HKDF(algorithm=self.hash_algorithm,length=self.key_length,salt=None,info=self.data_to_encode,).derive(shared_key)
+        return derived_key
+    
+    # And now we can demonstrate that the handshake performed in the
+    # opposite direction gives the same final value
+    
+    def generate_same_shared_key_with_peer_private_key(self, peer_private_key, server_private_key):
+        if peer_private_key and server_private_key:
+            same_shared_key = peer_private_key.exchange(ec.ECDH(), server_private_key.public_key())
+        return same_shared_key
+    
+
+    # Perform key derivation.
+
+    def perform_key_derivation_with_same_dervied_key(self, same_shared_key):
+        if same_shared_key:
+            same_derived_key = HKDF(algorithm=self.hash_algorithm,length=self.key_length,salt=None,info=self.data_to_encode,).derive(same_shared_key)
+        return same_derived_key
+
+    def check_key_derivation(self, derived_key, same_derived_key):
+        if derived_key == same_derived_key:
+            return True 
+        else:
+            return False
+
