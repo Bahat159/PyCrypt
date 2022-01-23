4e2a8d8684ca3ef1fe23c3fdba56401875a0bca4
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-23 01:39:04 -0600

    Update dsa.py

--- main/src/dsa.py
+++ main/src/dsa.py
@@ -1,5 +1,7 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import dsa
+from cryptography.hazmat.primitives.asymmetric import utils
+
 
 
 # DSA is a legacy algorithm and should generally be 
@@ -19,8 +21,32 @@ class DSA_Algoirthm:
             private_key = dsa.generate_private_key(key_size=self.key_size,)
         return private_key
     
+    # Generate Public Key
+    def generate_public_key(self, private_key, use_public_key = True):
+        if use_public_key:
+            public_key = private_key.public_key()
+        return public_key
+    
     # Message Signing
     def signature(self, data, use_signing = True):
         if use_signing:
             signature = private_key.sign(data,self.hash_algorithm)
         return signature
+    
+
+    def sign_big_data(self, private_key, data, use_big_data = True):
+        if use_big_data:
+            chosen_hash = self.hash_algorithm
+            hasher = hashes.Hash(chosen_hash)
+            hasher.update(data)
+            hasher.update(data)
+            digest = hasher.finalize()
+            sig = private_key.sign(digest,utils.Prehashed(chosen_hash))
+        return sig
+    
+    # Verification
+    def data_verification(self, public_key, signature, data, use_verification = True):
+        if use_verification:
+            public_key.verify(signature, data, self.hash_algorithm)
+        return public_key
+    
