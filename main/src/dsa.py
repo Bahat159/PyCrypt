ae42113f4b97875e54db2b6dffce5a074dedbd23
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-23 01:31:51 -0600

    Update dsa.py

--- main/src/dsa.py
+++ main/src/dsa.py
@@ -4,3 +4,22 @@ from cryptography.hazmat.primitives.asymmetric import dsa
 
 # DSA is a legacy algorithm and should generally be 
 # avoided in favor of choices like EdDSA using curve25519 or ECDSA.
+
+# DSA is a public-key algorithm for signing messages.
+
+class DSA_Algoirthm:
+    def __init__(self):
+        self.key_size = int('1024')
+        self.data     = bytes("this is some data I'd like to sign")
+        self.hash_algorithm = hashes.SHA256()
+    
+    def generate_privte_key(self, use_private_key = True):
+        if use_private_key:
+            private_key = dsa.generate_private_key(key_size=self.key_size,)
+        return private_key
+    
+
+    def signature(self, data, use_signing = True):
+        if use_signing:
+            signature = private_key.sign(data,self.hash_algorithm)
+        return signature
