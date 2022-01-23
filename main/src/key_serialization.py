96e3b16d0f22c44d562ab63136e110321ef6a873
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-23 08:20:05 -0600

    Update key_serialization.py

--- main/src/key_serialization.py
+++ main/src/key_serialization.py
@@ -10,7 +10,7 @@ class Key_Serialization:
         self.rsa_serialization_type = rsa.RSAPrivateKey
         self.dsa_serialization_type = dsa.DSAPrivateKey
 
-    def load_and_sign_pem_private_key(self, pem_data, message):
+    def load_and_serialize_pem_private_key(self, pem_data, message):
         key = load_pem_private_key(pem_data, password=self.password)
         rsa_serialize_type_check = check_data_type(key, self.rsa_serialization_type)
         dsa_serialize_type_check = check_data_type(key, self.dsa_serialization_type)
