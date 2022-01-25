e75453b53d9df89114520b50bade2ceba6e6f59c
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-25 09:14:40 -0600

    Update assymetric.py

--- main/src/assymetric.py
+++ main/src/assymetric.py
@@ -30,20 +30,24 @@ class Assymetric_Ed25519_signing():
         self.author         = 'Busari Habibullaah'
         self.description    = 'Ed25519 signing'
 
-    def generate_ed25519_private_key(self):
-        private_key = Ed25519PrivateKey.generate()
+    def generate_ed25519_private_key(self, use_generate_ed25519_private_key = True):
+        if use_generate_ed25519_private_key:
+            private_key = Ed25519PrivateKey.generate()
         return private_key
 
-    def sign_ed25519_(self, ed_generated_private_key, auth_message):
-        signature = ed_generated_private_key.sign(auth_message)
+    def sign_ed25519(self, ed_generated_private_key, auth_message, use_sign_ed25519_message = True):
+        if use_sign_ed25519_message:
+            signature = ed_generated_private_key.sign(auth_message)
         return signature
 
-    def generate_ed25519_public_key(self, ed_generated_private_key):
-        public_key = ed_generated_private_key.public_key()
+    def generate_ed25519_public_key(self, ed_generated_private_key, use_generate_public_key = True):
+        if use_generate_public_key:
+            public_key = ed_generated_private_key.public_key()
         return public_key
 
-    def sign_and_verify_public_key(self, public_key, signature, auth_message):
-        sign_and_verify = public_key.verify(signature, auth_message)
+    def sign_and_verify_public_key(self, public_key, signature, auth_message, use_sign_and_verify_key = True):
+        if use_sign_and_verify_key:
+            sign_and_verify = public_key.verify(signature, auth_message)
         return sign_and_verify
 
 class Assymetric_X25519PrivateKey():
@@ -61,49 +65,51 @@ class Assymetric_X25519PrivateKey():
     def __repr__(self):
         return self
 
-    def generate_X25519PrivateKey(self):
-        private_key =  X25519PrivateKey.generate()
-        if private_key:
-            return private_key 
+    def generate_X25519PrivateKey(self, use_generate_private_key = True):
+        if use_generate_private_key:
+            private_key =  X25519PrivateKey.generate()
+        return private_key 
 
-    def generate_peer_public_key_X25519PrivateKey(self):
+    def generate_peer_public_key_X25519PrivateKey(self, use_generate_public_key = True):
         # In a real handshake the peer_public_key will be received from the
         # other party. For this example we'll generate another private key and
         # get a public key from that. Note that in a DH handshake both peers
         # must agree on a common set of parameters.
-        public_key =  X25519PrivateKey.generate().public_key()
+        if use_generate_public_key:
+            public_key =  X25519PrivateKey.generate().public_key()
         return public_key
 
-    def generate_shared_key(self, peer_public_key):
-        if peer_public_key:
+    def generate_shared_key(self, peer_public_key, use_shared_key = True):
+        if use_shared_key:
             shared_key = private_key.exchange(peer_public_key)
             return shared_key
 
-    def derive_key(self, shared_key):
+    def derive_key(self, shared_key, use_derived_key = True):
         # Perform key derivation.
-        if shared_key:
-            derived_key = HKDF(algorithm=self.algorithm,length=int(self.key_length),salt=None,info=self.info,).derive(shared_key)
-            return derived_key
+        if use_derived_key:
+            derived_key = HKDF(algorithm=self.algorithm,length=self.key_length,salt=None,info=self.info,).derive(shared_key)
+        return derived_key
     
-    def private_key_2_for_handshake(self):
+    def private_key_2_for_handshake(self, use_private_key_handshake = True):
         # For the next handshake we MUST generate another private key
-        private_key_2 = X25519PrivateKey.generate()
+        if use_private_key_handshake:
+            private_key_2 = X25519PrivateKey.generate()
         return private_key_2
     
-    def public_key_2_handshake(self):
-        if self.generate_second_key:
+    def public_key_2_handshake(self, use_public_key_2_handshake = True):
+        if use_public_key_2_handshake:
             peer_public_key_2 = X25519PrivateKey.generate().public_key()
-            return peer_public_key_2
+        return peer_public_key_2
 
-    def shared_key_2(self, peer_public_key_2):
-        if peer_public_key_2:
+    def shared_key_2(self, peer_public_key_2, use_shared_key_2 = True):
+        if use_shared_key_2:
             shared_key_2 = private_key_2.exchange(peer_public_key_2)
-            return shared_key_2
+        return shared_key_2
 
-    def derived_key_2(self, shared_key_2):
-        if shared_key_2:
+    def derived_key_2(self, shared_key_2, use_derived_key_2 = True):
+        if use_derived_key_2:
             derived_key_2 = HKDF(algorithm=self.algorithm,length=self.key_length,salt=None,info=self.info,).derive(shared_key_2)
-            return derived_key_2
+        return derived_key_2
 
 
 class Ed448_signing_and_verification():
@@ -117,27 +123,27 @@ class Ed448_signing_and_verification():
     def __repr__(self):
         return self
     
-    def generate_private_key(self):
-        if self.signed_key:
+    def generate_private_key(self, use_generate_private_key = True):
+        if use_generate_private_key:
             private_key = Ed448PrivateKey.generate()
-            return private_key
+        return private_key
 
-    def sign_with_private_key(self, generated_private_key):
+    def sign_with_private_key(self, use_sign_with_private_key = True):
         # (bytes) – The signature to verify.
-        if generated_private_key:
+        if use_sign_with_private_key:
             signature = generated_private_key.sign(self.auth_message)
-            return signature
+        return signature
 
-    def generate_public_key(self):
-        if self.public_key:
+    def generate_public_key(self, use_generate_public_key = True):
+        if use_generate_public_key:
             public_key = private_key.public_key()
-            return public_key
-
-    def verify_public_key_with_message(self, signature, data):
-        # Raises InvalidSignature if verification fails
-        # data should be encoded in bytes()
-        if signature and data != "":
-            return  public_key.verify(signature, data)
+        return public_key
+    
+    # Raises InvalidSignature if verification fails
+    # data should be encoded in bytes()
+    def verify_public_key_with_message(self, signature, data, use_verify_with_public_key = True):
+        if use_verify_with_public_key:
+            return public_key.verify(signature, data)
 
 
 class X448_key_exchange():
@@ -158,48 +164,48 @@ class X448_key_exchange():
     def __repr__(self):
         return self
 
-    def generate_private_key(self):
-        if self.gen_private_key:
+    def generate_private_key(self, use_generate_private_key = True):
+        if use_generate_private_key:
             private_key = X448PrivateKey.generate()
-            return private_key
+        return private_key
 
     # In a real handshake the peer_public_key will be received from the
     # other party. For this example we'll generate another private key and
     # get a public key from that. Note that in a DH handshake both peers
     # must agree on a common set of parameters.
 
-    def generate_peer_public_key(self):
-        if self.gen_public_key:
+    def generate_peer_public_key(self, use_generate_peer_public_key = True):
+        if use_generate_peer_public_key:
             peer_public_key = X448PrivateKey.generate().public_key()
-            return peer_public_key
+        return peer_public_key
 
-    def agreed_shared_key(self, peer_public_key):
-        if peer_public_key:
+    def agreed_shared_key(self, peer_public_key, use_shared_key = True):
+        if use_shared_key:
             shared_key = private_key.exchange(peer_public_key)
-            return shared_key
+        return shared_key
 
-    def derived_key_derivation(self, shared_key):
-        if shared_key:
+    def derived_key_derivation(self, shared_key, use_key_derivation = True):
+        if use_key_derivation:
             derived_key = HKDF(algorithm=self.algorithm,length=self.key_length,salt=None,info=self.info,).derive(shared_key)
-            return derived_key
-
-    def generate_private_key_2(self):
-        # For the next handshake we MUST generate another private key.
-        private_key_2 = X448PrivateKey.generate()
-        if private_key_2:
-            return private_key_2
-
-    def generate_peer_public_key_2(self):
-        if self.gen_peer_public_key:
+        return derived_key
+    
+    # For the next handshake we MUST generate another private key.
+    def generate_private_key_2(self, use_private_key_2 = True):
+        if use_private_key_2:
+            private_key_2 = X448PrivateKey.generate()
+        return private_key_2
+        
+    def generate_peer_public_key_2(self, use_generate_peer_public_key_2 = True):
+        if use_generate_peer_public_key_2:
             peer_public_key_2 = X448PrivateKey.generate().public_key()
-            return peer_public_key_2
+        return peer_public_key_2
 
-    def generate_shared_key(self, peer_public_key_2):
-        if peer_public_key_2:
+    def generate_shared_key(self, peer_public_key_2, use_shared_key_2 = True):
+        if use_shared_key_2:
             shared_key_2 = private_key_2.exchange(peer_public_key_2)
-            return shared_key_2
+        return shared_key_2
 
-    def dervied_key_2(self, shared_key_2):
-        if shared_key_2:
+    def dervied_key_2(self, shared_key_2, use_derived_key_2 = True):
+        if use_derived_key_2:
             derived_key_2 = HKDF(algorithm= self.algorithm,length=self.key_length,salt=None,info=self.info,).derive(shared_key_2)
-            return derived_key_2
+        return derived_key_2
