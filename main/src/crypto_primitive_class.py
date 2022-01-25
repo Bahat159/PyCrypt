a4e07e8c53355f722d6ccec4e781e5e626df8934
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-25 08:45:59 -0600

    Update crypto_primitive_class.py

--- main/src/crypto_primitive_class.py
+++ main/src/crypto_primitive_class.py
@@ -2,25 +2,111 @@ import os
 import sys
 import base64
 from colorama import init
+from colorama import Fore, Back, Style
+from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-#from cryptography.hazmat.primitives.ciphers.aead import AESOCB3
 from cryptography.hazmat.primitives.ciphers.aead import AESCCM
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+
 
 # copyright, Sandcroft Software, 2021.
 # implementing encryption algorithms OCD( Authenticated encryption (AEAD) )
 
+# Library features 
+# encrypt filetypes (exe,pdf,jpg,txt,svg)
+
+# program flow
+# generate a new token
+# make an encryption key
+# encrypt data
+# all major variables must accept command from config to change at will
 
 #   BASED ON OCB algo
 # OCB is a blockcipher-based mode of operation that simultaneously provides both privacy and authenticity for a user-supplied plaintext
 # the algorithm is efficiently realizable by smart cards, FPGAs, and high-speed ASICs
 
+#   ANOTHER RELIABLE METHOD TO ACHIEVE PRIVACY
+
+# In the past, when one wanted a shared-key mechanism providing both privacy and authenticity
+# the usual thing to do was to separately encrypt and compute a MAC, using two different keys
+#  (The word MAC stands for Message Authentication Code.)
+
+# If one is encrypting and MACing in a conventional way,
+# like CTR-mode encryption and the CBC MAC
+# the cost for privacy-and-authenticity is going to be twice the cost for privacy alone
+# just counting blockcipher calls.
+#
+
 #   NOTETHIS 
+
 # The encryption is what buys you privacy and the MAC is what gets you authenticity.
 # The cost to get privacy-and-authenticity in this way is about the cost to encrypt (with a privacy-only scheme) plus the cost to MAC.
+ 
+init(autoreset=True)
+
+myFile = "./fsm.txt"
+class EncryptWithFernet():
+    def __init__(file_name):
+        myFile = file_name
+        self.key_length = int('32')
+        self.iterations = int('320000')
+        self.encoding_type = hashes.SHA256()
+    
+    def __repr__(self):
+        return self
+
+    def generate_new_token():
+        passcode = Fernet.generate_key()
+        return passcode
+
+    def print_red(message):
+        print(f"{Fore.RED}{message}")
+
+    def print_success(message):
+        print(f"{Back.GREEN}{message}")
+
+    def make_new_encryption_key(passcode):
+        salt = os.urandom(16)kdf = PBKDF2HMAC(algorithm=self.encoding_type,length=self.key_length,salt=salt,iterations=self.iterations,backend=default_backend())
+        key = base64.urlsafe_b64encode(kdf.derive(passcode))
+        return Fernet(key)
+
+
+    def encrypt_data(key_frame,message):
+        encrypted_token = key_frame.encrypt(bytes(message,  encoding='utf8'))
+        return encrypted_token
+
+    def decrypt_data(key_frame,message):
+        decrypted_token = key_frame.decrypt(message)
+        return decrypted_token
+
+    def file_content_to_encrypt(myFile):
+        with open(myFile,'r') as new_content:
+            file_content = new_content.read().replace('\r', '')
+            return file_content
+
+    def show_decrypted_file_content(newKey,myFile):
+        if newKey:
+            dec_data = decrypt_data(newKey, myFile)
+            return dec_data
+
+    def show_encrypted_file_content(newKey,myFile):
+        if newKey:
+            enc_data = encrypt_data(newKey, myFile)
+            return enc_data
+
+
+    def encrypt_file_content(newKey, myFile):
+        file_content = file_content_to_encrypt(myFile)
+        enc_data = encrypt_data(newKey, file_content)
+        return enc_data
+
+    def decrypt_file_content(newKey, myFile):
+        decrypt_me = file_content_to_encrypt(myFile)
+        dec_data = decrypt_data(newKey, decrypt_me)
+        return dec_data
 
 
 class HazardousMaterialLayer_AEAD_ChaCha20Poly1305():
@@ -38,12 +124,15 @@ class HazardousMaterialLayer_AEAD_ChaCha20Poly1305():
     #  an approach that would yield a longer ciphertext.
 
     def __init__(self, print_class, associated_data):
-        self.print_class_callback          = print_class  # pretty printing class init
+        self.print_class_callback          = print_class
         self.associated_data_param         = associated_data
         self.hazardous_material_layer_data = [" Library alert mode "]
         self.description                   = "OCB AEAD ChaCha Cipher Encryption Implementation"
         self.author                        = "Busari Habibullaah"
-        self.nonce                         = os.urandom(12)
+        self.nonce                         = os.urandom(int('12'))
+    
+    def __repr__(self):
+        return self
 
     def _generate_new_chacha20Poly1305_key_(self):
         # Securely generates a random ChaCha20Poly1305 key.
@@ -83,7 +172,8 @@ class HazardousMaterialLayer_AEAD_ChaCha20Poly1305():
         except Exception:
             raise InvalidTag('\n{0} an authenticated encryption tag fails to verify during decryption'.format(algor))
         return decrypt
-      
+
+
 class HazardousMaterialLayer_AEAD_AESGCM():
 
     # OCB solves the problem of nonce-based authenticated-encryption with associated-data (AEAD) 
@@ -99,8 +189,11 @@ class HazardousMaterialLayer_AEAD_AESGCM():
     def __init__():
         self.description                   = "OCB AEAD AESGCM Cipher Encryption Implementation"
         self.author                        = "Busari Habibullaah"
-        self.nonce                         = os.urandom(12)
-        self.bit_length                    = '256'
+        self.nonce                         = os.urandom(int('12'))
+        self.bit_length                    = int('256')
+    
+    def __repr__(self):
+        return self
 
     def generate_aesgcm_key(self, bit_length):
         # bit_length can be 128, 192, or 256-bit key. This must be kept secret
@@ -148,7 +241,11 @@ class HazardousMaterialLayer_AEAD_AESOCB3():
         self.description                   = "OCB AEAD AESOCB3 Cipher Encryption Implementation"
         self.author                        = "Busari Habibullaah"
         self.nonce                         = os.urandom(12)
-        self.bit_length                    = '256'
+        self.bit_length                    = int('256')
+    
+
+    def __repr__(self):
+        return self
 
     def generate_aesocb3_key(self):
         key = AESOCB3.generate_key(self.bit_length)
@@ -166,13 +263,15 @@ class HazardousMaterialLayer_AEAD_AESOCB3():
         decrypt_aesocb_data = aesocb_generated_key.decrypt(nonce, encrypt_data, associated_data)
         return decrypt_aesocb_data
 
-      
 class HazardousMaterialLayer_AEAD_AESCCM():
     def __init__(self):
         self.description                   = "OCB AEAD AESCCM Cipher Encryption Implementation"
         self.author                        = "Busari Habibullaah"
         self.nonce                         = os.urandom(12)
-        self.bit_length                    = '256'          # key length (128, 192, 256 bit-key)
+        self.bit_length                    = int('256')         # key length (128, 192, 256 bit-key)
+    
+    def __repr__(self):
+        return self
 
     def generate_aesocb3_key(self):
         key = AESCCM.generate_key(self.bit_length)
@@ -189,4 +288,46 @@ class HazardousMaterialLayer_AEAD_AESCCM():
         nonce = self.nonce
         decrypt_aesccb_data = aesccm_generated_key.decrypt(nonce, encrypt_data, associated_data)
         return decrypt_aesccb_data
-   
+
+
+class PrintWithFormat():
+    def __init__(self):
+        self.data    = []
+        self.new_key = str("MY")
+        self.decode  = str("DE")
+    
+    def __repr__(self):
+        return self
+
+    def print_with_format(self,new_key, decode):
+        print_format = "\n New ChaCha key => {} \n Decoded String with Base64() {}\n"
+        print(print_format.format(new_key, decode))
+    
+    def print_format_with_index_number(self,new_key, decode):
+        print_format_with_index_number = "\n New ChaCha key => {0} \n Decoded String with Base64() {1}\n"
+        print(print_format_with_index_number.format(new_key, decode))
+
+    def print_with_named_index(self, new_key, decode):
+        # TODOHERE
+
+        # not working properly 
+        # need to be fixed to work with Default Argument Values
+        print_with_named_index = "\n New ChaCha key => {new_key} \n Decoded String with Base64() {decode}\n"
+        print(print_with_named_index.format(new_key, decode))
+
+    def print_text(message):
+        print(f'{message}')
+    
+    def initiate_module():
+        try:
+            print_class = PrintWithFormat()
+            class_method = HazardousMaterialLayer_AEAD(print_class)
+            returned_key = class_method._generate_new_chacha20Poly1305_key_(print_class)
+            key = returned_key['new_key']
+            decoder = returned_key['decode']
+
+            print('\nNew Key = [{0}]\nBase64decode = [{1}]\n'.format(key, decoder), end=" ")
+        except Exception as err:
+            print(f'\nException occured in operation ==> {err}')
+            sys.exit('App exiting ....')
+
