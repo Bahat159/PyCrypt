b61203941f5504936cd6beaf3c18d5fea7875f8f
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-26 04:11:42 -0600

    Update symmetric.py

--- main/src/symmetric.py
+++ main/src/symmetric.py
@@ -20,38 +20,54 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 # you should evaluate Fernet to see if it fits your needs before implementing anything using this module.
 
 # Example Usage
+#class_object = symmertic_Encryption()
+#encrypt = class_object.do_AES_encryption()
+#decrypt = class_object.do_AES_decryption(encrypt)
 #
-# class_object = symmertic()
-# encrypt = class_object.do_encryption()
-# decrypt = class_object.do_decryption(encrypt)
+#print(encrypt)
+# b'\x19\x0c\x93\x1c\xffx\x8d*zv\xe7\x97\x12\xb3\xed\xad'
 #
-# print(encrypt)
-#b'\xf2\x93\x14\xc4:\xe4\xb8\xc6\xabzP\x8e\xca\x81(_'
-#
-# print(decrypt)
+#print(decrypt)
 # b'a secret message'
 
 
-class symmertic:
+class symmertic_Encryption:
     # Cipher objects combine an algorithm such as AES with a mode like CBC or CTR. 
     # A simple example of encrypting and then decrypting content with AES is:
 
     def __init__(self):
+        self.chahcha_nonce = os.urandom(int('16'))
         self.key_length = os.urandom(int('32'))  # (32, 128, 192, 156)
         self.key_iv     = os.urandom(int('16'))
         self.secret_message = bytes("a secret message", encoding = "utf8")
-        self.cipher     = Cipher(algorithms.AES(self.key_length), modes.CBC(self.key_iv))
+        self.AES_cipher     = Cipher(algorithms.AES(self.key_length), modes.CBC(self.key_iv))
     
     def __repr__(self):
         return self
     
-    def do_encryption(self):
-        encryptor = self.cipher.encryptor()
+    def do_AES_encryption(self):
+        encryptor = self.AES_cipher.encryptor()
         ct = encryptor.update(self.secret_message) + encryptor.finalize()
         return ct
     
-    def do_decryption(self, ct):
-        decryptor = self.cipher.decryptor()
+    def do_AES_decryption(self, ct):
+        decryptor = self.AES_cipher.decryptor()
         return decryptor.update(ct) + decryptor.finalize()
-        
+    
 
+    # ChaCha Algorithm
+
+    def do_chacha_encryption(self, key, use_chacha_encryptor = True):
+        if use_chacha_encryptor:
+            algorithm = algorithms.ChaCha20(key, self.chahcha_nonce)
+            cipher = Cipher(algorithm, mode=None)
+            encryptor = cipher.encryptor()
+            ct = encryptor.update(self.secret_message)
+        return ct
+    
+    def do_chacha_decryption(self, ct, use_chacha_decryptor = True):
+        if use_chacha_decryptor:
+            algorithm = algorithms.ChaCha20(key, self.chahcha_nonce)
+            cipher = Cipher(algorithm, mode=None)
+            decryptor = cipher.decryptor()
+        return decryptor.update(ct)
